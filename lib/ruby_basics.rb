ef1a044fc0d7bdb3c6b957ce2dcf2110d861bed5
christy8059 <christyl8059@hstat.org>
2017-10-11 02:14:13 +0000

    fix all 8 errors

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(num1,num2)
+    num1/num2
 end
 
-def argue
-  
+def argue(argurment)
+  phrase = "I'm right and you are wrong!"
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(toppings= "cheese")
+toppings
 end
 
-def whisper
-    
+def whisper(word)
+    word.downcase
 end
